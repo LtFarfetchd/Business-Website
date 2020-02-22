d8fddeac3a18b5ce585a0734ee4129e3d35eb890
Adam Judd <adam.judd@outlook.com.au>
2020-02-22 18:00:41 +1100

    Updates

---
+++ precache-manifest.569a13a1e896248cd75cf42de1a7bdea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f28fab89bb4ca69f9833ba1c3aef13",
+    "url": "/LtFarfetchd/Business-Website/index.html"
+  },
+  {
+    "revision": "9db43e21be319b4ae854",
+    "url": "/LtFarfetchd/Business-Website/static/js/2.c088d39c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/LtFarfetchd/Business-Website/static/js/2.c088d39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1acc76a2bdfaa6dbdb",
+    "url": "/LtFarfetchd/Business-Website/static/js/main.93d2896e.chunk.js"
+  },
+  {
+    "revision": "d41678827c84e5bb8e57",
+    "url": "/LtFarfetchd/Business-Website/static/js/runtime-main.c541da3b.js"
+  }
+]);
